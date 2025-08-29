db87d6ab9e272ff8fa94be0832a70c8c216e755a
Lalo Caballero <laloworld94@gmail.com>
2025-08-29 10:43:04 +0200

    wsgi not proper lines

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,13 @@
 import eventlet
-from app import create_app
 eventlet.monkey_patch()
 
-# The factory now returns both objects
+from app import create_app
+
+# The factory returns both objects
 app, socketio = create_app()
+
+# This is what Gunicorn needs to access
+application = app  # Gunicorn looks for 'application' by default
 
 if __name__ == "__main__":
     # This allows you to run "python wsgi.py" for local testing
